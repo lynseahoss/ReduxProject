cdbc0d8680009bb78bef69d21c03122eed728607
Lindsey Lawson <lynseahoss@gmail.com>
2020-11-23 22:03:40 -0500

    added provider & createstore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={createStore()}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
