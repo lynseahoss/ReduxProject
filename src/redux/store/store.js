3c9400dc47cf8ce70e7c9eaca2bc4cf0c3b01d6e
Lindsey Lawson <lynseahoss@gmail.com>
2020-11-24 12:31:05 -0500

    removed []s from ...middleware

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -6,7 +6,7 @@ const reducer = combineReducers({})
 const initialState = {}
 const middleware = [thunk]
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware([...middleware])))
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
 
 export default store 
