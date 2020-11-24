fa06521b898bad0f0040ceac311b1f9df6af595b
Lindsey Lawson <lynseahoss@gmail.com>
2020-11-24 18:32:43 -0500

    imported header & searchbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 const App=() => {
   return (
     <div>
-     
+      <Header/>
+     <SearchBar/>
     </div>
   );
 }
