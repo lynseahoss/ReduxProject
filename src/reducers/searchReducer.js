8684d64aa2db3bf1912bfe29e8e4456cadaa158e
Lindsey Lawson <lynseahoss@gmail.com>
2020-11-24 13:35:33 -0500

    created searchReducer.js

---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,42 @@
+import { SEARCH_TERM_REQUEST, SEARCH_TERM_SUCESS,SEARCH_TERM_FAIL } from '../constants/termConstants';
+
+export const searchTermReducer = (state ={term:[]}, action)=>{
+    switch(action.type){
+        case SEARCH_TERM_REQUEST:
+        return { loading: true, terms: [] }
+        case SEARCH_TERM_SUCESS:
+        return{ loading: false, terms: action.payload }
+        case SEARCH_TERM_FAIL:
+        return{ loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { SEARCH_TERM } from '../actions/index';
+
+// const SearchReducer =(userTerm = [], action)=>{
+//     switch (action.type){
+//         case SEARCH_TERM:
+//         return [...userTerm, action.payload.term]
+//     }
+//     return userTerm
+// }
+
+// export default SearchReducer
