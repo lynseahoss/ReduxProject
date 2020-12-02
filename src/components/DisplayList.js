d7a57d24c9a7494e2de4e475e1a0bd77f323f0c9
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-02 14:22:59 -0500

    added termSearch action

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { termSearch } from '../actions/searchTermAction';
 
 const DisplayList = () => {
     return (
         <div className="container">
         <div className="block">
-  This text is within a <strong>block</strong>.
+    <h3>{termSearch.title}</h3>
+    {termSearch.author}
+    {termSearch.url}
+    {termSearch.comments}
 </div>
 </div>
     );
