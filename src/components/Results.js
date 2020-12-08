7402c88cfeb420133b2210e2238d0b0c918736d8
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-08 17:47:51 -0500

    search results in the works

---
+++ src/components/Results.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Results = () => {
+    return (
+        <div className="columns">
+        <div className="column is-three-fifths is-offset-one-fifth">
+  <section className="section">
+    <div className="container">
+      <h2 className="title">Section</h2>
+      <h3 className="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+      </h3>
+    </div>
+  </section>
+</div>
+</div>
+
+    );
+}
+
+export default Results;
