f997f09a34a3230d68c185014e39aaeb6693b55d
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-08 17:49:28 -0500

    created Home page/ added state & effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import Header from '../components/Header';
-import DisplayList from '../components/DisplayList';
+import React, { useState, useEffect} from 'react';
+import hackerURL from '../api/hackerURL';
 import SearchBar from '../components/SearchBar';
+import Header from '../components/Header';
+import Results from '../components/Results';
+
+
+
 const Home = () => {
+    const [search, setSearch] = useState([])
+
+    useEffect(()=>{
+        onSearchSubmit('')
+    },[])
+
+    const onSearchSubmit = async(term)=>{
+        const response = await hackerURL.get('/search?&tags=story', {
+            params:{
+                query:term
+            }
+        })
+        setSearch(response.data.hits)
+    }
+
     return (
         <div>
             <Header/>
-            <SearchBar/>
-            <DisplayList/>
+            <SearchBar onFormSubmit={onSearchSubmit}/>
+            <Results search={search}/>
         </div>
     );
 }
