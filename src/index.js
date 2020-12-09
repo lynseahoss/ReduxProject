111f0abaf8d680727fbef1bcae40ae76e27ff311
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-08 20:09:51 -0500

    imported store & Provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import  store  from './store/store';
+import  store  from './redux/store/store';
 
 import './index.css';
 import App from './App';
@@ -9,9 +9,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
  
-    <Provider store={store}>
-    <App />
-    </Provider>,
+
+    <App />,
 
   document.getElementById('root')
 );
