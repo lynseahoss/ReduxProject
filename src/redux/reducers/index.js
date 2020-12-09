975080504d38637a9333cd630711ca772146076c
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-09 13:08:44 -0500

    created history reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,29 @@
-import { SEARCH_REQUEST, SEARCH_SUCESS, SEARCH_FAIL } from "../constants/index";
+import {
+  SEARCH_REQUEST,
+  SEARCH_SUCESS,
+  SEARCH_FAIL,
+  SAVE_HISTORY
+} from "../constants/index";
 
 export const searchReducer = (state = { search: [] }, action) => {
   switch (action.type) {
     case SEARCH_REQUEST:
-      return { loading: true, search: [] };
+      return { loading: true, result: [] };
     case SEARCH_SUCESS:
       return { loading: false, search: action.payload };
     case SEARCH_FAIL:
       return { loading: false, error: action.payload };
 
+    default:
+      return state;
+  }
+};
+
+export const historyReducer = (state = [], action) => {
+  switch (action.type) {
+    case SAVE_HISTORY:
+      return [ action.payload, ...state ];
+   
     default:
       return state;
   }
