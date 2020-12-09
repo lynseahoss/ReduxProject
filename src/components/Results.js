f70883d801055a8f6a8bc60a69191d80dc6eedc4
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-09 15:03:55 -0500

    added terinary for loading, error for results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { useSelector} from "react-redux";
-
+import { useSelector } from "react-redux";
 
 import ResultItem from "./ResultItem";
 
 const Results = () => {
-
- const resultList = useSelector((state)=>state.search)
-
+  const resultList = useSelector((state) => state.search);
+  const { loading, error } = resultList;
 
   const searchResult = resultList.search.map((searchResult) => {
-    return <ResultItem key={searchResult.objectID} searchResult={searchResult} />;
+    return (
+      <ResultItem key={searchResult.objectID} searchResult={searchResult} />
+    );
   });
 
   return (
@@ -18,9 +18,13 @@ const Results = () => {
       <div className="column is-three-fifths is-offset-one-fifth">
         <section className="section">
           <div className="container">
-           
-              {searchResult}
-            
+            {loading ? (
+              <h2 className="title">Loading...</h2>
+            ) : error ? (
+              <h2 className="subtitle">{error}</h2>
+            ) : (
+              [searchResult]
+            )}
           </div>
         </section>
       </div>
