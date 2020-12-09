a24e6cc50f24af2ebed28fed9abeaf2e9e970b2e
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-09 13:10:49 -0500

    added story url

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const ResultItem = ({ searchResult }) => {
+
+    
     return (
         <div>
         <h2 className="subtitle mb-0">Article: <strong>{searchResult.title}</strong></h2>
-      <h3 className="subtitle">
+      <h3 className="subtitle mb-0">
        Author: {searchResult.author}
+      </h3>
+      <h3 className="subtitle mb-0">
+       URL: <a href={searchResult.url} target="blank">{searchResult.url}</a>
       </h3>
       <br/>
         </div>
