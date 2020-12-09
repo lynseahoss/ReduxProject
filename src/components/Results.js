05b200e3a5d8da517b1f893d4eb50d9d99a69805
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-09 13:11:39 -0500

    working on rendering result list

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import ResultItem from './ResultItem';
-const Results = ({ search }) => {
-    
-        const resultList = search.map(searchResult=>{
-          return(
-            <ResultItem
-            key={searchResult.title}
-            searchResult={searchResult}
-            />
-          )
-        })
-return(
-        <div className="columns">
-        <div className="column is-three-fifths is-offset-one-fifth">
-  <section className="section">
-    <div className="container">
-     {resultList}
-    </div>
-  </section>
-</div>
-</div>
+import React from "react";
+import { useSelector } from "react-redux";
 
-    );
-}
+import ResultItem from "./ResultItem";
+
+const Results = ({search}) => {
+
+  // const search = useSelector((state) => state.search);
+  // const { loading, error, result } = search;
+
+
+  const searchResult = search.map((searchResult) => {
+    return <ResultItem key={searchResult.objectID} searchResult={searchResult} />;
+  });
+
+  return (
+    <div className="columns">
+      <div className="column is-three-fifths is-offset-one-fifth">
+        <section className="section">
+          <div className="container">
+           
+              {searchResult}
+            
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Results;
