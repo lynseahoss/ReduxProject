7f3ba38aa2fb3d1f12603ec6aa073761b3caf268
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-09 13:50:11 -0500

    changed result [] to search []

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -8,7 +8,7 @@ import {
 export const searchReducer = (state = { search: [] }, action) => {
   switch (action.type) {
     case SEARCH_REQUEST:
-      return { loading: true, result: [] };
+      return { loading: true, search: [] };
     case SEARCH_SUCESS:
       return { loading: false, search: action.payload };
     case SEARCH_FAIL:
