b374fb25457553cb19547d12f070723f77a9e4e8
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-23 14:21:17 -0500

    imported Link from RRD

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav className="navbar" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand">
-    <a className="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-    </a>
-
-    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-      <a className="navbar-item">
-        Home
-      </a>
-
-      <a className="navbar-item">
-        Documentation
-      </a>
-
-      <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">
-          More
-        </a>
-
-        
-      </div>
-    </div>
-
-    <div className="navbar-end">
-      <div className="navbar-item">
-        
-      </div>
-    </div>
-  </div>
-</nav>
+  return (
+    <nav
+      className="navbar is-white"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <div className="navbar-item" to="/">
+        HackerNews
         </div>
-    );
-}
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+         
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+            <Link className="navbar-item" to="/topstories">
+             Top Stories
+            </Link>
+            <Link className="navbar-item" to="/entertainment">
+              Entertainment
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
