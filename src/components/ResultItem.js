1eae5a2f8ba4ff60205edf5f2352061ab8362079
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-23 14:27:48 -0500

    added a tag to title and removed url h3

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -5,12 +5,9 @@ const ResultItem = ({ searchResult }) => {
     
     return (
         <div>
-        <h2 className="subtitle mb-0">Article: <strong>{searchResult.title}</strong></h2>
+        <h2 className="subtitle mb-0">Article: <strong><a href={searchResult.url} target="blank">{searchResult.title}</a></strong></h2>
       <h3 className="subtitle mb-0">
        Author: {searchResult.author}
-      </h3>
-      <h3 className="subtitle mb-0">
-       URL: <a href={searchResult.url} target="blank">{searchResult.url}</a>
       </h3>
       <br/>
         </div>
