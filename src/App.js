0819922401c0b53a3a8898df59f14dac6a8573d1
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-23 14:17:04 -0500

    imported RRD & NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import NavBar from './components/Navbar';
 import Home from './pages/Home';
 
 
 const App=() => {
   return (
-<Home/>
+<Router>
+<NavBar/>
+<main className="py-3">
+        <div className="container">
+          <Route path="/" component={Home} exact/>
+        
+        </div>
+      </main>
+</Router>
   );
 }
 
