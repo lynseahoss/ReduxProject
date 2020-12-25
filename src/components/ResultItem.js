5f8679f69afb1d9a778057dc0519c2d4cb79b6d8
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-24 22:35:10 -0500

    removed commented out code

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -5,11 +5,7 @@ const ResultItem = ({ searchResult }) => {
     
     return (
         <div>
-        <h2 className="subtitle mb-0">Article: <strong><a href={searchResult.url} target="blank">{searchResult.title}</a></strong></h2>
-      {/* <h3 className="subtitle mb-0">
-       Author: {searchResult.author}
-      </h3>
-      <br/> */}
+        <h2 className="subtitle mb-0"> <strong>Article: <a href={searchResult.url} target="blank">{searchResult.title}</a></strong></h2>
       <br/>
         </div>
     );
