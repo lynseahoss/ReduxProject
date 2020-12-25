41bb607197e1016a7b0c67a1fe0f5465d6bd794c
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-24 21:45:07 -0500

    commented out Author

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -6,9 +6,10 @@ const ResultItem = ({ searchResult }) => {
     return (
         <div>
         <h2 className="subtitle mb-0">Article: <strong><a href={searchResult.url} target="blank">{searchResult.title}</a></strong></h2>
-      <h3 className="subtitle mb-0">
+      {/* <h3 className="subtitle mb-0">
        Author: {searchResult.author}
       </h3>
+      <br/> */}
       <br/>
         </div>
     );
