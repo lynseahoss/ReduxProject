22bedb85c50d717361cdb2cc5560441ebd5dca5c
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-29 17:04:28 -0500

    made a page for frontpage

---
+++ src/pages/FrontPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FrontPage from '../components/FrontPage';
+
+const FrontPageNews = () => {
+    
+    return (
+        <div>
+            <FrontPage/>
+        </div>
+    );
+}
+
+export default FrontPageNews;
