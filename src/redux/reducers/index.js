93d5592ad52699f0268face878eb4ec0bd935366
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-29 17:05:52 -0500

    created frontpage reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,11 @@ import {
   SEARCH_REQUEST,
   SEARCH_SUCESS,
   SEARCH_FAIL,
-  SAVE_HISTORY
+  SAVE_HISTORY,
+  FRONT_STORY_REQUEST,
+  FRONT_STORY_SUCESS,
+  FRONT_STORY_FAIL,
+  RENDER_HISTORY
 } from "../constants/index";
 
 export const searchReducer = (state = { search: [] }, action) => {
@@ -22,6 +26,29 @@ export const historyReducer = (state = [], action) => {
   switch (action.type) {
     case SAVE_HISTORY:
       return [action.payload, ...state];
+    default:
+      return state;
+  }
+};
+
+
+export const renderHistoryReducer = (state = [], action) => {
+  switch (action.type) {
+    case RENDER_HISTORY:
+      return [action.payload, ...state];
+    default:
+      return state;
+  }
+};
+
+export const frontPageReducer = (state = { search: [] }, action) => {
+  switch (action.type) {
+    case FRONT_STORY_REQUEST:
+      return { loading: true, search: [] };
+    case FRONT_STORY_SUCESS:
+      return { loading: false, search: action.payload };
+    case FRONT_STORY_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
