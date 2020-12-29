1f60ea05d4b11ed31321c2e06bd878c769bbda8a
Lindsey Lawson <lynseahoss@gmail.com>
2020-12-29 17:54:18 -0500

    added useselector to get from entertainment state

--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -2,7 +2,13 @@ import React, {useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { entertainmentAction } from '../redux/actions';
+
 const Entertainment = () => {
+
+    const entertainment = useSelector((state) => state.entertainment);
+    const dispatch = useDispatch();
+    const { loading, error, search } = entertainment;
+
     return (
         <div>
             
